22bffe10e18c4aaecd8bcc21befe6000994cc57d
혜원 <152113818+hey5nee123@users.noreply.github.com>
2023-12-11 17:23:03 +0900

    ㅇ
    ㅇ

--- 6.sql
+++ 6.sql
@@ -68,3 +68,5 @@ SELECT  e.last_name,e.job_id,d.department_name,e.salary,j.grade_level
 FROM    employees e, departments d,job_grades j
 WHERE   e.department_id = d.department_id
 AND     e.salary BETWEEN j.lowest_sal AND j.highest_sal;
+
+DESC job_grades;
